8bd7cf39ac54b6380f7b174f49401dd46b577b06
Mark Tyrkba <awfulmarco@gmail.com>
2025-01-28 20:16:36 +0300

    Build DAG graph and use `fxhash`

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 use std::fs::{self, File};
 use std::process::{Command, ExitCode};
-use std::collections::{HashMap, HashSet};
 
 use memmap2::Mmap;
+use fxhash::{FxHashMap, FxHashSet};
+
+type StrHashSet<'a> = FxHashSet::<&'a str>;
+type StrHashMap<'a, T> = FxHashMap::<&'a str, T>;
+
+type Graph<'a> = StrHashMap::<'a, StrHashSet<'a>>;
 
 const RUSH_FILE_NAME: &str = "build.rush";
 
@@ -140,9 +145,9 @@ struct Def<'a> {
 
 #[derive(Debug, Default)]
 struct Parsed<'a> {
-    jobs: HashMap::<&'a str, Job<'a>>,
-    defs: HashMap::<&'a str, Def<'a>>,
-    rules: HashMap::<&'a str, Rule<'a>>,
+    jobs: StrHashMap::<'a, Job<'a>>,
+    defs: StrHashMap::<'a, Def<'a>>,
+    rules: StrHashMap::<'a, Rule<'a>>,
 }
 
 #[derive(Debug, Default, PartialEq)]
@@ -232,28 +237,97 @@ impl<'a> Parser<'a> {
     }
 }
 
+fn build_dependency_graph<'a>(parsed: &'a Parsed) -> Graph<'a> {
+    let mut graph = StrHashMap::default();
+
+    for (target, job) in &parsed.jobs {
+        let dependencies = job.inputs.iter().cloned().collect();
+        graph.insert(target, dependencies);
+    }
+
+    graph
+}
+
+#[cfg(debug_assertions)]
+fn is_dag(graph: &Graph) -> bool {
+    let n = graph.len();
+    let mut stack = StrHashSet::default(); stack.reserve(n);
+    let mut visited = StrHashSet::default(); visited.reserve(n);
+
+    fn dfs<'a>(node: &'a str, graph: &Graph<'a>, visited: &mut StrHashSet<'a>, stack: &mut StrHashSet<'a>) -> bool {
+        if stack.contains(node) { return false }
+        if visited.contains(node) { return true }
+
+        stack.insert(node);
+        visited.insert(node);
+
+        if let Some(dependencies) = graph.get(node) {
+            for dep in dependencies {
+                if !dfs(dep, graph, visited, stack) {
+                    return false
+                }
+            }
+        }
+
+        stack.remove(node);
+        true
+    }
+
+    for node in graph.keys() {
+        if !dfs(node, graph, &mut visited, &mut stack) {
+            return false
+        }
+    }
+
+    true
+}
+
+fn topological_sort<'a>(graph: &Graph<'a>) -> Vec::<&'a str> {
+    let n = graph.len();
+    let mut ret = Vec::with_capacity(n);
+    let mut visited = StrHashSet::default(); visited.reserve(n);
+
+    fn dfs<'a>(node: &'a str, graph: &Graph<'a>, visited: &mut StrHashSet<'a>, ret: &mut Vec::<&'a str>) {
+        if visited.contains(node) { return }
+
+        visited.insert(node);
+
+        graph.get(node).map(|deps| {
+            deps.iter().for_each(|dep| dfs(dep, graph, visited, ret));
+        });
+
+        ret.push(node)
+    }
+
+    for node in graph.keys() {
+        dfs(node, graph, &mut visited, &mut ret)
+    }
+
+    ret
+}
+
 fn resolve_and_build<'a>(
     job: &Job<'a>,
     parsed: &Parsed<'a>,
-    commands: &mut Vec::<String>,
-    built_outputs: &mut HashSet::<&'a str>,
+    built: &mut StrHashSet<'a>,
+    commands: &mut Vec::<String>
 ) {
-    if built_outputs.contains(job.target) {
+    if built.contains(job.target) {
         println!("{target} is already built", target = job.target);
         return
     }
 
     for input in job.inputs.iter() {
         if let Some(dep_job) = parsed.jobs.get(input) {
-            resolve_and_build(dep_job, parsed, commands, built_outputs)
-        } else if !built_outputs.contains(input) {
+            resolve_and_build(dep_job, parsed, built, commands)
+        } else if !built.contains(input) {
             println!("dependency {input} is assumed to exist")
         }
     }
 
     if let Some(rule) = parsed.rules.get(job.rule) {
         let command = rule.template.compile(job, parsed);
-        built_outputs.insert(job.target);
+        built.insert(job.target);
         commands.push(command)
     } else {
         eprintln!("no rule found for job: {target}", target = job.target)
@@ -268,26 +342,46 @@ fn main() -> ExitCode {
     let content = to_str(&rush[..]);
     let parsed = Parser::parse(content);
 
-    let mut commands = Vec::new();
-    let mut built_outputs = HashSet::new();
+    let graph = build_dependency_graph(&parsed);
 
-    for job in parsed.jobs.values() {
-        resolve_and_build(job, &parsed, &mut commands, &mut built_outputs)
+    #[cfg(debug_assertions)]
+    if !is_dag(&graph) {
+        eprintln!("Error: Dependency graph contains cycles");
+        return ExitCode::FAILURE
+    }
+
+    let build_order = topological_sort(&graph);
+    println!("build order:");
+    print!("{}", build_order[0]);
+    build_order[1..].iter().for_each(|b| {
+        print!(" -> {b}")
+    });
+    println!();
+
+    let n = build_order.len();
+    let mut built = StrHashSet::default(); built.reserve(n);
+    let mut commands = Vec::with_capacity(n);
+
+    for target in build_order {
+        if let Some(job) = parsed.jobs.get(target) {
+            resolve_and_build(job, &parsed, &mut built, &mut commands);
+        }
     }
 
     for command in commands.iter() {
         println!("{command}");
-        let out = Command::new("sh").arg("-c")
+        let Ok(out) = Command::new("sh")
+            .arg("-c")
             .arg(command)
-            .output()
-            .unwrap();
+            .output() else {
+                return ExitCode::FAILURE
+            };
 
         let out = if out.status.success() {
             unsafe { std::str::from_utf8_unchecked(&out.stdout) }
         } else {
             unsafe { std::str::from_utf8_unchecked(&out.stderr) }
         };
-
         print!("{out}")
     }
 
