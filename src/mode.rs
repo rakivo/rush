ac1d0262e7f73a00a2b2d1b820edce66da6fc264
Mark Tyrkba <awfulmarco@gmail.com>
2025-02-07 16:20:10 +0300

    Completely automated the way of adding new flags in `mode` module

--- src/mode.rs
+++ src/mode.rs
@@ -1,45 +1,51 @@
-use flager::{Flag, Parser as FlagParser, new_flag};
-
-const QUIET_MODE: Flag = new_flag!("-q", "--quiet");
-const VERBOSE_MODE: Flag = new_flag!("-v", "--verbose");
-const ALWAYS_BUILD_MODE: Flag = new_flag!("-B", "--always-build");
-
-#[repr(packed)]
-#[cfg_attr(feature = "dbg", derive(Debug))]
-pub struct Mode {
-    quiet: bool,
-    verbose: bool,
-    always_build: bool
-}
-
 macro_rules! getter {
-    ($($f: ident: $ty: ty), *) => {
+    ($($f: ident: $bit: path), *) => {
         $(#[inline(always)]
-        pub fn $f(&self) -> $ty {
-            self.$f
+        pub fn $f(&self) -> bool {
+            (self.0 & (1 << $bit as u64)) != 0
         })*
-    };
+    }
 }
 
 macro_rules! new {
-    ($($flag: ident = $method: ident($($arg: expr), *)), *) => {
+    ($($flag: expr => $method: ident($($arg: expr), *)), *) => {
         #[inline(always)]
-        pub fn new(flag_parser: &FlagParser) -> Self {
-            Self { $($flag: flag_parser.$method($($arg)*)), * }
+        pub fn new(flag_parser: &flager::Parser) -> Self {
+            let mut mode = 0;
+            $(if flag_parser.$method($($arg)*) {
+                mode |= (1 << $flag as u64)
+            })*
+            Self(mode)
         }
-    };
+    }
 }
 
-impl Mode {
-    getter!{
-        quiet: bool,
-        verbose: bool,
-        always_build: bool
-    }
+macro_rules! define_flags {
+    ($([$short: ident, $name: ident]), *) => { paste::paste! {
+        $(
+            const [<$name:snake:upper _MODE>]: flager::Flag = flager::Flag::new(
+                concat!("-", stringify!($short)),
+                concat!("--", stringify!($name)),
+                None
+            );
+        )*
 
-    new!{
-        quiet = passed(&QUIET_MODE),
-        verbose = passed(&VERBOSE_MODE),
-        always_build = passed(&ALWAYS_BUILD_MODE)
-    }
+        #[repr(u64)]
+        enum FlagBit { $([<$name:camel>]), * }
+
+        #[repr(transparent)]
+        #[cfg_attr(feature = "dbg", derive(Debug))]
+        pub struct Mode(u64);
+
+        impl Mode {
+            getter! { $($name: FlagBit:: [<$name:camel>]), * }
+            new! { $(FlagBit:: [<$name:camel>] => passed(&[<$name:snake:upper _MODE>])), * }
+        }
+    }}
+}
+
+define_flags! {
+    [q, quiet],
+    [v, verbose],
+    [B, always_build]
 }
